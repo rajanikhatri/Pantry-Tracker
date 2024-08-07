d832413e4293edd43f2713d54f17733e8d5a2e8a
Rajani Khatri <rajanikhatri8876@gmail.com>
2024-08-07 12:26:14 -0400

    fix windows not defined issues

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 require('dotenv').config();
 //console.log(process.env);
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//
 const firestore = getFirestore(app);
 
 export { firestore };
